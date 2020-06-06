fca2c8165e3237391417dcfdc9d6253689c381b9
Oleksandr Kazanzhy <oleksandr.kazanzhy@gmail.com>
2020-06-06 22:54:28 +0200

    Creating Project + testing some stuff

--- WebProject/ClientApp/src/components/NavMenu.js
+++ WebProject/ClientApp/src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -26,19 +27,20 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">WebProject</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Ticket Tracker</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/list">List of Workers</NavLink>
+                </NavItem>
+
               </ul>
             </Collapse>
           </Container>
---
+++ WebProject/Models/WorkerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProject.Models
+{
+    public class Worker
+    {
+        public long Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Position { get; set; }
+        public string Work_Phone_Number { get; set; }
+        public string Work_Email { get; set; }
+        public string Name { get; set; }
+        public string Sirname { get; set; }
+        public string PESEL { get; set; }
+        public string Date_of_Birth { get; set; }
+        public string Date_of_Employment { get; set; }
+        public bool Valid { get; set; }
+    }
+}
